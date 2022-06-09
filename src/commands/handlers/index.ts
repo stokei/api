9178047cc1af7b2a2996c438adb9100d7d0e5da0
Stokei Oficial <stokeioficial@gmail.com>
2022-06-08 22:23:28 -0300

    Add eslintignore and sort-imports

--- src/commands/handlers/index.ts
+++ src/commands/handlers/index.ts
@@ -1,132 +1,69 @@
 import { AccessCommandHandlers } from './accesses';
 import { AccountCommandHandlers } from './accounts';
-import { ProjectCommandHandlers } from './projects';
-
-import { ProjectsMemberCommandHandlers } from './projects-members';
-
-import { ProjectsPlanCommandHandlers } from './projects-plans';
-
-import { SiteCommandHandlers } from './sites';
-
-import { SitesLightColorCommandHandlers } from './sites-light-colors';
-
-import { SitesDarkColorCommandHandlers } from './sites-dark-colors';
-
-import { DomainCommandHandlers } from './domains';
-
-import { PageCommandHandlers } from './pages';
-
-import { MetatagCommandHandlers } from './metatags';
-
-import { TagCommandHandlers } from './tags';
-
-import { CurrencyCommandHandlers } from './currencies';
-
-import { LanguageCommandHandlers } from './languages';
-
-import { KeywordCommandHandlers } from './keywords';
-
-import { VersionCommandHandlers } from './versions';
-
-import { ColorCommandHandlers } from './colors';
-
 import { ActivityCommandHandlers } from './activities';
-
 import { ActivitiesActionCommandHandlers } from './activities-actions';
-
-import { CategoryCommandHandlers } from './categories';
-
-import { CheckoutCommandHandlers } from './checkouts';
-
-import { CheckoutsCurrencyCommandHandlers } from './checkouts-currencies';
-
-import { ProductCommandHandlers } from './products';
-
-import { ProductsCategoryCommandHandlers } from './products-categories';
-
-import { ProductsImageCommandHandlers } from './products-images';
-
-import { PriceCommandHandlers } from './prices';
-
-import { ProductsTagCommandHandlers } from './products-tags';
-
-import { OrderCommandHandlers } from './orders';
-
-import { OrdersItemCommandHandlers } from './orders-items';
-
-import { OrdersAddressCommandHandlers } from './orders-addresses';
-
-import { OrdersSellerCommandHandlers } from './orders-sellers';
-
-import { PaymentCommandHandlers } from './payments';
-
-import { PaymentsMethodCommandHandlers } from './payments-methods';
-
-import { CardCommandHandlers } from './cards';
-
-import { CartCommandHandlers } from './carts';
-
-import { CartsItemCommandHandlers } from './carts-items';
-
-import { PlanCommandHandlers } from './plans';
-
-import { ImageCommandHandlers } from './images';
-
-import { RatingCommandHandlers } from './ratings';
-
-import { CommentCommandHandlers } from './comments';
-
-import { QuestionCommandHandlers } from './questions';
-
-import { AnswerCommandHandlers } from './answers';
-
-import { VideoCommandHandlers } from './videos';
-
-import { VideosTagCommandHandlers } from './videos-tags';
-
-import { VideosAuthorCommandHandlers } from './videos-authors';
-
-import { VideosSubtitleCommandHandlers } from './videos-subtitles';
-
-import { FileCommandHandlers } from './files';
-
-import { ModuleCommandHandlers } from './modules';
-
-import { ModulesVideoCommandHandlers } from './modules-videos';
-
-import { ModulesMaterialCommandHandlers } from './modules-materials';
-
-import { VideosMaterialCommandHandlers } from './videos-materials';
-
-import { CourseCommandHandlers } from './courses';
-
-import { CoursesInstructorCommandHandlers } from './courses-instructors';
-
-import { CoursesAdminCommandHandlers } from './courses-admins';
-
-import { CoursesStudentCommandHandlers } from './courses-students';
-
-import { ClassroomCommandHandlers } from './classrooms';
-
-import { ClassroomsStudentCommandHandlers } from './classrooms-students';
-
-import { ClassroomsEnrollmentCommandHandlers } from './classrooms-enrollments';
-
-import { ClassroomsAdminCommandHandlers } from './classrooms-admins';
-
-import { ClassroomsInstructorCommandHandlers } from './classrooms-instructors';
-
-import { ClassroomsPlanCommandHandlers } from './classrooms-plans';
-
-import { ClassroomsModuleCommandHandlers } from './classrooms-modules';
-
-import { ClassroomsTagCommandHandlers } from './classrooms-tags';
-
-import { ClassroomsMaterialCommandHandlers } from './classrooms-materials';
-
 import { AddressCommandHandlers } from './addresses';
-
+import { AnswerCommandHandlers } from './answers';
+import { CardCommandHandlers } from './cards';
+import { CartCommandHandlers } from './carts';
+import { CartsItemCommandHandlers } from './carts-items';
+import { CategoryCommandHandlers } from './categories';
+import { CheckoutCommandHandlers } from './checkouts';
+import { CheckoutsCurrencyCommandHandlers } from './checkouts-currencies';
+import { ClassroomCommandHandlers } from './classrooms';
+import { ClassroomsAdminCommandHandlers } from './classrooms-admins';
+import { ClassroomsEnrollmentCommandHandlers } from './classrooms-enrollments';
+import { ClassroomsInstructorCommandHandlers } from './classrooms-instructors';
+import { ClassroomsMaterialCommandHandlers } from './classrooms-materials';
+import { ClassroomsModuleCommandHandlers } from './classrooms-modules';
+import { ClassroomsPlanCommandHandlers } from './classrooms-plans';
+import { ClassroomsStudentCommandHandlers } from './classrooms-students';
+import { ClassroomsTagCommandHandlers } from './classrooms-tags';
+import { ColorCommandHandlers } from './colors';
+import { CommentCommandHandlers } from './comments';
+import { CourseCommandHandlers } from './courses';
+import { CoursesAdminCommandHandlers } from './courses-admins';
+import { CoursesInstructorCommandHandlers } from './courses-instructors';
+import { CoursesStudentCommandHandlers } from './courses-students';
+import { CurrencyCommandHandlers } from './currencies';
+import { DomainCommandHandlers } from './domains';
+import { FileCommandHandlers } from './files';
+import { ImageCommandHandlers } from './images';
+import { KeywordCommandHandlers } from './keywords';
+import { LanguageCommandHandlers } from './languages';
+import { MetatagCommandHandlers } from './metatags';
+import { ModuleCommandHandlers } from './modules';
+import { ModulesMaterialCommandHandlers } from './modules-materials';
+import { ModulesVideoCommandHandlers } from './modules-videos';
+import { OrderCommandHandlers } from './orders';
+import { OrdersAddressCommandHandlers } from './orders-addresses';
+import { OrdersItemCommandHandlers } from './orders-items';
+import { OrdersSellerCommandHandlers } from './orders-sellers';
+import { PageCommandHandlers } from './pages';
+import { PaymentCommandHandlers } from './payments';
+import { PaymentsMethodCommandHandlers } from './payments-methods';
 import { PhoneCommandHandlers } from './phones';
+import { PlanCommandHandlers } from './plans';
+import { PriceCommandHandlers } from './prices';
+import { ProductCommandHandlers } from './products';
+import { ProductsCategoryCommandHandlers } from './products-categories';
+import { ProductsImageCommandHandlers } from './products-images';
+import { ProductsTagCommandHandlers } from './products-tags';
+import { ProjectCommandHandlers } from './projects';
+import { ProjectsMemberCommandHandlers } from './projects-members';
+import { ProjectsPlanCommandHandlers } from './projects-plans';
+import { QuestionCommandHandlers } from './questions';
+import { RatingCommandHandlers } from './ratings';
+import { SiteCommandHandlers } from './sites';
+import { SitesDarkColorCommandHandlers } from './sites-dark-colors';
+import { SitesLightColorCommandHandlers } from './sites-light-colors';
+import { TagCommandHandlers } from './tags';
+import { VersionCommandHandlers } from './versions';
+import { VideoCommandHandlers } from './videos';
+import { VideosAuthorCommandHandlers } from './videos-authors';
+import { VideosMaterialCommandHandlers } from './videos-materials';
+import { VideosSubtitleCommandHandlers } from './videos-subtitles';
+import { VideosTagCommandHandlers } from './videos-tags';
 
 export const CommandHandlers = [
   ...AccessCommandHandlers,

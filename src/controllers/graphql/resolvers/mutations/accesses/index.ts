ab72337e6d4c31e837f6b98d5c7fd589b2a8ad8f
Stokei Oficial <stokeioficial@gmail.com>
2022-06-10 09:09:46 -0300

    Remove unnecessary accesses mutations

--- src/controllers/graphql/resolvers/mutations/accesses/index.ts
+++ src/controllers/graphql/resolvers/mutations/accesses/index.ts
@@ -1,9 +1,3 @@
-import { CreateAccessResolver } from './create-access';
 import { RemoveAccessResolver } from './remove-access';
-import { UpdateAccessResolver } from './update-access';
 
-export const AccessesMutations = [
-  CreateAccessResolver,
-  RemoveAccessResolver,
-  UpdateAccessResolver
-];
+export const AccessesMutations = [RemoveAccessResolver];

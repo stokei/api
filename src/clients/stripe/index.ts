f317262c8e5b4707ef763ec35b84bc9a58954c53
Stokei Oficial <stokeioficial@gmail.com>
2023-03-28 18:38:09 -0300

    Change stripe version

--- src/clients/stripe/index.ts
+++ src/clients/stripe/index.ts
@@ -3,6 +3,6 @@ import Stripe from 'stripe';
 import { STRIPE_SECRET_KEY } from '@/environments';
 
 export const stripeClient = new Stripe(STRIPE_SECRET_KEY, {
-  apiVersion: '2022-08-01',
+  apiVersion: '2022-11-15',
   typescript: true
 });

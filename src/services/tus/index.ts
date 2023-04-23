9b52355446adfb4fe799e245bd961eb373a65b64
Stokei Oficial <stokeioficial@gmail.com>
2023-04-23 19:23:14 -0300

    TusService is only to development

--- src/services/tus/index.ts
+++ src/services/tus/index.ts
@@ -1,3 +1,5 @@
+import { IS_DEVELOPMENT } from '@/environments';
+
 import { TusService } from './tus-service';
 
-export const TusServices = [TusService];
+export const TusServices = IS_DEVELOPMENT ? [TusService] : [];

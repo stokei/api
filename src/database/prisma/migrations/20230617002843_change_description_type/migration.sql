2dc4630b48b58829e72d5e82910b3bb9d26438f7
Douglas <stokeioficial@gmail.com>
2023-06-16 21:45:41 -0300

    Change description type

---
+++ src/database/prisma/migrations/20230617002843_change_description_type/migration.sql
@@ -0,0 +1,20 @@
+-- AlterTable
+ALTER TABLE `apps` MODIFY `description` MEDIUMTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `courses` MODIFY `description` MEDIUMTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `features` MODIFY `description` MEDIUMTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `modules` MODIFY `description` MEDIUMTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `plans` MODIFY `description` MEDIUMTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `products` MODIFY `description` MEDIUMTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `videos` MODIFY `description` MEDIUMTEXT NULL;

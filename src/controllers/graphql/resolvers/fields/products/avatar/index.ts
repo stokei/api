dc6e3bcb5c34f42eb82215353cd45d781881a378
Douglas <stokeioficial@gmail.com>
2023-08-30 00:31:20 -0300

    Fix product image

--- src/controllers/graphql/resolvers/fields/products/avatar/index.ts
+++ src/controllers/graphql/resolvers/fields/products/avatar/index.ts
@@ -3,6 +3,7 @@ import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
 import { ImagesLoader } from '@/controllers/graphql/dataloaders/images.loader';
 import { Image } from '@/controllers/graphql/types/image';
 import { Product } from '@/controllers/graphql/types/product';
+import { ImageModel } from '@/models/image.model';
 import { ProductModel } from '@/models/product.model';
 import { FindProductParentByIdParentService } from '@/services/products/find-product-parent-by-parent';
 
@@ -15,11 +16,20 @@ export class ProductAvatarResolver {
 
   @ResolveField(() => Image, { nullable: true })
   async avatar(@Parent() product: ProductModel) {
+    let image: ImageModel;
+    if (product.avatar) {
+      image = await this.imagesLoader.findByIds.load(product.avatar);
+    }
+    if (image || product.parent) {
+      return;
+    }
     const parent = await this.findProductParentByIdParentService.execute(
       product.parent
     );
-    return this.imagesLoader.findByIds.load(
-      product.avatar || (parent as any)?.avatar
-    );
+    const parentAvatar = (parent as any)?.avatar;
+    if (!parentAvatar) {
+      return;
+    }
+    return this.imagesLoader.findByIds.load(parentAvatar);
   }
 }
